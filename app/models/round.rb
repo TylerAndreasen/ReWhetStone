859dc5c8bb49d8489065113e6ac381944c53f31d
TylerAndreasen <45052003+TylerAndreasen@users.noreply.github.com>
2024-11-27 16:35:24 -0700

    Tweaked Round views to return to the player page, rather than the non-existent player round index page. NOTE:: Last commit also added the DOCS directory, a list of documentation files about what I am doing within the server and how I am doing it. More should be added in the future, but we will see how motiviated I remain to do this.

--- app/models/round.rb
+++ app/models/round.rb
@@ -4,8 +4,8 @@ class Round < ApplicationRecord
   # The Round model describes the format of the data needed to represent
   # a round of axe throwing scores that the user can submit. It is my intent
   # to allow users to only submit rounds that have all scores input to them,
-  # based on an appropariate UI. I believe that this will be easier to
-  # implment if I only allow submission of complete rounds, and rely on JS
+  # based on an appropriate UI. I believe that this will be easier to
+  # implement if I only allow submission of complete rounds, and rely on JS
   # (specifically P5.js) to handle reading the rather complicated input.
   # The first link below is a highly useful explanation of how to use JS
   # to make HTML form submissions. The second link is the formal explanation
@@ -17,15 +17,15 @@ class Round < ApplicationRecord
   # In short, the first four columns of the Round record are the same, each
   # takes the form of being a score of 1, 3, 5, or a Drop.
   # The final Called Clutch, throw can take the same form as the first, or
-  # can be have the form of 7, of a Drop. Which list of valid options the
+  # can be have the form of 7, or a Drop. Which list of valid options the
   # Called Clutch shot takes is based on whether the player calls clutch
   # before throwing and inputs yes on the appropriate part of the JS [NYI].
-  # This called clutch flag of course must be true or false.
+  # This called clutch field must be either "Stay Down" or "Call Clutch", the
+  # two options in the official rules.
 
   VALID_NORMAL_THROWS = ["1", "3", "5", "Drop"]
   VALID_CLUTCH_THROWS = ["7", "Drop"]
 
-  # TO BE REMOVED LATER
   ALL_VALID_THROWS = ["1", "3", "5", "7", "Drop"]
   BOOLEAN_STRINGS = ["Stay Down", "Call Clutch"]
 
@@ -43,23 +43,23 @@ class Round < ApplicationRecord
   validates :score_4, inclusion: {in: VALID_NORMAL_THROWS, message: "%{value} is not a valid score."}
   validates :called_clutch, inclusion: {in: BOOLEAN_STRINGS, message: "%{value} is not a clutch call value."}
 
-  # Unsure if this validation will operate correctly, it seems like it should.
-  # Though logically only having one does potentially allow invalid values when the clutch is called atm.
-  # I also don't know if white space is a problem.
-  # https://guides.rubyonrails.org/v8.0/active_record_validations.html#using-a-symbol-with-if-and-unless
-  validates :score_5, 
-      inclusion: 
-      {in: VALID_NORMAL_THROWS, message: "%{value} is not a valid clutch score."},
-          if: :select_clutch?
+  # New direction, `with_options`. This is based on information from the below
+  # link, and seems a little more appropriate for the intended use case.
+  # https://medium.com/unagi/active-record-validations-in-ruby-on-rails-grouping-conditional-validations-2e1c8fbba464
 
-
+  with_options if: :select_clutch? do |clutch_valid|
+    clutch_valid.validates :score_5, inclusion: {in: VALID_CLUTCH_THROWS, message: "%{value} is not a valid clutch score."}
+  end
+  with_options if: :select_down? do |down_valid|
+    down_valid.validates :score_5, inclusion: {in: VALID_NORMAL_THROWS, message: "%{value} is not a valid down score."}
+  end
 
   def select_clutch?
-      called_clutch == BOOLEAN_STRINGS[0]
+      called_clutch == BOOLEAN_STRINGS[1]
   end
 
   def select_down?
-      called_clutch == BOOLEAN_STRINGS[1]
+      called_clutch == BOOLEAN_STRINGS[0]
   end
 
 end
